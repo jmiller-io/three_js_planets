51b9934ba4f767c94f9af1e65a728eb91f5a09ea
Jake Miller <hillsighed@gmail.com>
2016-12-31 16:10:59 -0800

    modifies clouds and changes shininess of spec map

--- main.js
+++ main.js
@@ -4,45 +4,53 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize (window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement);
 
-var light = new THREE.AmbientLight( 0x888888, 1.5)
+var light = new THREE.AmbientLight( 0x888888)
 scene.add( light )
 
-  var light = new THREE.DirectionalLight( 0xcccccc, .3)
-  light.position.set(5,5,5)
-  scene.add( light )
+var light = new THREE.DirectionalLight( 0xcccccc, 1)
+light.position.set(5,3,5)
+scene.add( light )
 
 // Planet Earth
-var bmap =  THREE.ImageUtils.loadTexture('images/earthmap1k.jpg', {}, function(){});
+var loader = new THREE.TextureLoader();
+var texture1 = loader.load('images/earthmap1k.jpg');
+var texture2 = loader.load('images/earthspec1k.jpg');
+var texture3 = loader.load('images/cloud_map_with_trans.jpg');
+
 var geometry = new THREE.SphereGeometry( 0.5, 32, 32 );
 var material = new THREE.MeshPhongMaterial({
-  map: THREE.ImageUtils.loadTexture('images/earthmap1k.jpg'),
-  bumpMap: bmap,
+  map: texture1,
+  bumpMap: texture1,
   bumpScale: 0.05,
-  specularMap: THREE.ImageUtils.loadTexture('images/earthspec1k.jpg'),
-  specular: new THREE.Color('grey')
+  specularMap: texture2,
+  specular: new THREE.Color('grey'),
+  shininess: 10
 });
 var earthMesh = new THREE.Mesh( geometry, material );
 scene.add( earthMesh );
 
 
 // Create the clouds
-// var geometry = new THREE.SphereGeometry(0.503, 32, 32)
-// var material = new THREE.MeshPhongMaterial ({
-//   color: 0xffffff,
-//   map: THREE.ImageUtils.loadTexture( 'images/earthcloudmaptrans.jpg' ),
-//   transparent: true,
-// })
-// var clouds = new THREE.Mesh( geometry, material);
-// earthMesh.add( clouds );
+var geometry = new THREE.SphereGeometry(0.503, 32, 32)
+var material = new THREE.MeshPhongMaterial ({
+  map: texture3,
+  side: THREE.DoubleSide,
+  opacity: 0.275,
+  transparent: true,
+  depthWrite: false
+})
+var clouds = new THREE.Mesh( geometry, material);
+earthMesh.add( clouds );
 
 
 // sets distance of camera from planet
-camera.position.z = 1;
+camera.position.z = 1.5;
 
 function render() {
   requestAnimationFrame (render);
   renderer.render( scene, camera);
-  // earthMesh.rotation.y += .003;
+  earthMesh.rotation.y += .002;
+  //clouds.rotation.y += .002;
 };
 
 render();
